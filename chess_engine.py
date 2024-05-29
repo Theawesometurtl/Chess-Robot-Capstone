9a373030452f9a3d71d95b20d7298a8cbaacc005
Theawesometurtl <shablammmo@gmail.com>
2024-05-29 15:19:13 -0700

    chessboard logic

--- chess_engine.py
+++ chess_engine.py
@@ -1,27 +1,69 @@
 import chess
 import chess.engine
+import random
 
 engine = chess.engine.SimpleEngine.popen_uci("./stockfish/stockfish-android-armv8")
-limit = chess.engine.Limit(time=0.001)
+limit = chess.engine.Limit(time=0.1)
 board = chess.Board()
-result = engine.play(board, limit)
 
-print(board)
-print()
-
+def engine_move():
+    result = engine.play(board, limit)
+    board.push(result.move)
+    
+def check_state():
+    if board.is_stalemate():
+        print("stalemate")
+        engine.quit()
+        return True
+    elif board.is_insufficient_material():
+        print("insufficient material")
+        engine.quit()
+        return True
+    elif board.can_claim_fifty_moves():
+        print("fifty move rule")
+        engine.quit()
+        return True
+    elif board.can_claim_threefold_repitition():
+        print("threefoldrepitition")
+        engine.quit()
+        return True
+    return False
+    
+    
+    
+print("Do you want to play black, white, or random")
+while True:
+    player_colour = input()
+    if player_colour == "random":
+        player_colour = random.choice(["white","black"])
+        print(player_colour)
+    if player_colour == "white":
+        PB = False
+        break
+    elif player_colour == "black":
+        PB = True
+        break
+    else:
+        print("invalid input")
+    print(board)
+    print()
+if PB:
+    engine_move()
+    print(board)
+    print()
 while True:
     try:
-        x = input()
-        if x == "stop":
+        move = input()
+        if move == "stop":
             engine.quit()
             break
-        board.push_san(x)
+        board.push_san(move)
         print(board)
         print()
-        result = engine.play(board, limit)
-        board.push(result.move)
-        print(result.move)
+        engine_move()
         print(board)
         print()
+        if check_state():
+            break
     except:
         print("wrong notation try again")
