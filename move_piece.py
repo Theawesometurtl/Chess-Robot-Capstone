597201d2918491a47672c306a62d52d0d259691c
Dylan-Lively <dyl.lively@gmail.com>
2024-06-01 16:19:52 -0700

    side flip

--- move_piece.py
+++ move_piece.py
@@ -15,13 +15,16 @@ def move_piece(x1: int, y1, x2: int, y2: int):
     move_claw(home.x, home.y)
 
 
+flip_num = { 0 : 7, 1 : 6, 2 : 5, 3 : 4, 4 : 3, 5 : 2, 6 : 1, 7 : 0}
 
 
-
-def get_distance_and_angle(square):
+def get_distance_and_angle(square, bot_colour):
     X = ord(square[0]) - 97
     Y = int(square[1]) - 1
     print("coordinates = " + str(X) + "," + str(Y))
+
+    if bot_colour == "black":
+        X , Y = flip_num[X] , flip_num[Y]
 
     rX = X - 3.5
     rY = Y + 1.5 # distance from center of motor to middle of first row of squares (in square size, eg. center of motor right on edge of board = 0.5) (multiple by size of one square later)
@@ -32,4 +35,4 @@ def get_distance_and_angle(square):
     print("distance = " + str(round(distance , 2)) + " squares")
     print("angle = " + str(round(angle , 2)) + " degrees")
 
-get_distance_and_angle("h8")
+get_distance_and_angle("a1" , "black")
