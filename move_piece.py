c5caeaef607e0e1e1e067c70725f59b0d2acf833
Dylan-Lively <dyl.lively@gmail.com>
2024-06-01 15:56:24 -0700

    get_distance_and_angle function

--- move_piece.py
+++ move_piece.py
@@ -1,3 +1,5 @@
+import math
+
 def move_piece(x1: int, y1, x2: int, y2: int):
     home = {x: -5, y: -5}
     lift_claw()
@@ -11,3 +13,23 @@ def move_piece(x1: int, y1, x2: int, y2: int):
     open_claw()
     lift_claw()
     move_claw(home.x, home.y)
+
+
+
+
+
+def get_distance_and_angle(square):
+    X = ord(square[0]) - 97
+    Y = int(square[1]) - 1
+    print("coordinates = " + str(X) + "," + str(Y))
+
+    rX = X - 3.5
+    rY = Y + 1.5 # distance from center of motor to middle of first row of squares (in square size, eg. center of motor right on edge of board = 0.5) (multiple by size of one square later)
+
+    distance = math.sqrt(rX ** 2 + rY ** 2)
+    angle = math.degrees(math.atan(rX / rY)) + 90 # from 0 degrees (all the way left) to 180 degrees (all the way right)
+
+    print("distance = " + str(round(distance , 2)) + " squares")
+    print("angle = " + str(round(angle , 2)) + " degrees")
+
+get_distance_and_angle("h8")
