39f131a8421f0435c4bfb30a3b336bb76e7be0e9
Dylan-Lively <dyl.lively@gmail.com>
2024-06-01 17:51:46 -0700

    coordinate to motor movement

--- move_piece.py
+++ move_piece.py
@@ -1,4 +1,6 @@
 import math
+import chess
+import chess.engine
 
 def move_piece(x1: int, y1, x2: int, y2: int):
     home = {x: -5, y: -5}
@@ -16,9 +18,9 @@ def move_piece(x1: int, y1, x2: int, y2: int):
 
 
 flip_num = { 0 : 7, 1 : 6, 2 : 5, 3 : 4, 4 : 3, 5 : 2, 6 : 1, 7 : 0}
+bot_colour =  "white"
 
-
-def get_distance_and_angle(square, bot_colour):
+def get_distance_and_angle(square):
     X = ord(square[0]) - 97
     Y = int(square[1]) - 1
     print("coordinates = " + str(X) + "," + str(Y))
@@ -35,4 +37,35 @@ def get_distance_and_angle(square, bot_colour):
     print("distance = " + str(round(distance , 2)) + " squares")
     print("angle = " + str(round(angle , 2)) + " degrees")
 
-get_distance_and_angle("a1" , "black")
+def move_motors(move):
+    sq1 = move[0] + move[1]
+    sq2 = move[2] + move[3]
+    Wsq3 = move[2] + str(int(move[3]) - 1)
+    Bsq3 = move[2] + str(int(move[3]) + 1)
+
+    if board.is_capture(move):
+        if board.is_en_passant(move):
+            if turn_colour == "white":
+                capture_sq = Wsq3
+            else:
+                capture_sq = Bsq3
+        else:
+            capture_sq = sq2
+        
+        get_distance_and_angle(capture_sq)
+        grab_piece(board.piece_at(chess.capture_sq))
+        move_to_piece_storage()
+        release_piece(board.piece_at(chess.capture_sq))
+
+
+    get_distance_and_angle(sq1)
+    grab_piece(board.piece_at(chess.sq1))
+    get_distance_and_angle(sq2)
+    release_piece(board.piece_at(chess.sq1))
+    defalt_position()
+
+def grab_piece(piece):
+    pass
+
+def release_piece(piece):
+    pass
